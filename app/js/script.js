bea970cbf4bfbde05239d669a9ecd8a9ee5a4611
ehsanmavaei <ehsanmavaei@gmail.com>
2022-05-05 23:14:12 +0430

    add basic form validations

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,38 @@
-console.log('HELLO');
+console.log("HELLO");
 
 const test = () => {
-	console.log('this is a test');
+  console.log("this is a test");
 };
+const namealert = document.getElementById("namealert");
+const parent = document.getElementsByClassName("name")[0];
+const lastNamealert = document.getElementById("lastnamealert");
+const emailalert = document.getElementById("emailalert");
+const passwordalert = document.getElementById("passwordalert");
+function checkForms() {
+  const name = document.getElementById("name");
+  const lastName = document.getElementById("last");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  if (name.value == "") {
+    name.parentElement.classList.add("show");
+  } else {
+    name.parentElement.classList.remove("show");
+  }
+  if (lastName.value == "") {
+    lastName.parentElement.classList.add("show");
+  } else {
+    lastName.parentElement.classList.remove("show");
+  }
+  if (email.value == "") {
+    email.parentElement.classList.add("show");
+  } else {
+    email.parentElement.classList.remove("show");
+  }
+  if (password.value == "") {
+    password.parentElement.classList.add("show");
+  } else {
+    password.parentElement.classList.remove("show");
+  }
+
+  console.log(name, lastName, email, password);
+}
