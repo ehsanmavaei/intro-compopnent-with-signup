01fd3c51a91e1bc919426f2bd19af7271ccfae03
ehsanmavaei <ehsanmavaei@gmail.com>
2022-05-06 18:39:27 +0430

    refatore the functionality

--- app/js/script.js
+++ app/js/script.js
@@ -3,16 +3,16 @@ console.log("HELLO");
 const test = () => {
   console.log("this is a test");
 };
-const namealert = document.getElementById("namealert");
-const parent = document.getElementsByClassName("name")[0];
-const lastNamealert = document.getElementById("lastnamealert");
+
 const emailalert = document.getElementById("emailalert");
-const passwordalert = document.getElementById("passwordalert");
+const img = document.getElementsByClassName("email")[0].children[2];
+
 function checkForms() {
   const name = document.getElementById("name");
   const lastName = document.getElementById("last");
   const email = document.getElementById("email");
   const password = document.getElementById("password");
+
   if (name.value == "") {
     name.parentElement.classList.add("show");
   } else {
@@ -23,11 +23,13 @@ function checkForms() {
   } else {
     lastName.parentElement.classList.remove("show");
   }
+
   if (email.value == "") {
     email.parentElement.classList.add("show");
   } else {
     email.parentElement.classList.remove("show");
   }
+
   if (password.value == "") {
     password.parentElement.classList.add("show");
   } else {
@@ -35,4 +37,21 @@ function checkForms() {
   }
 
   console.log(name, lastName, email, password);
+}
+
+
+
+function checkemail() {
+  var re = /\S+@\S+\.\S+/;
+  console.log("email finction is being called");
+
+  if (re.test(email.value)) {
+    emailalert.innerHTML = "valid eamil";
+    emailalert.style.color = "green";
+    img.style.opacity = 0;
+  } else {
+    emailalert.innerHTML = "email is not valid";
+    emailalert.style.opacity = 1;
+    img.style.opacity = 1;
+  }
 }
